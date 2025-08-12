d7d325eb6ee6dd0a4f91b97bddb5d2672d980dd7
Rejinrajeev <rejn.raj.rajeev@gmail.com>
2025-08-12 15:47:06 +0530

    commit after working condition chances left

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-const users = [];
+let tasks = [];
 let new_task = document.getElementById('new_task');
 let submit = document.getElementById('submit_btn');
 let task_list = document.querySelector('.task_list');
 let warning = document.getElementById('warning');
-
-submit.addEventListener('click', function () {
-    if (new_task.value == '') {
-        warning.innerHTML = 'Empty Field';
-    }
-    else {
-        warning.innerHTML = '';
+function display() {
+    task_list.innerHTML = '';
+    tasks.forEach((task, index) => {
         let list_item = document.createElement('li');
         list_item.classList.add('li_flex');
         let task_text = document.createElement('span');
-        task_text.textContent = new_task.value;
+        task_text.textContent = task.Name;
         let checker = document.createElement('input');
         checker.type = ('checkbox');
         let delete_btn = document.createElement('button');
         delete_btn.textContent = 'Delete';
         delete_btn.classList.add('btn')
         delete_btn.addEventListener('click', function () {
-            list_item.remove();
-            users.pop();
+            tasks.splice(index, 1);
+            display();
         });
+        checker.addEventListener('change', () => {
+            let checker_value = checker.checked;
+                task.checker=checker_value;
+        })
         list_item.append(checker, task_text, delete_btn);
         task_list.append(list_item);
-        users.push({ task: new_task.value, checker: false });
-        new_task.value = '';
+    })
+}
+submit.addEventListener('click', function () {
+    if (new_task.value == '') {
+        warning.innerHTML = 'Empty Field';
     }
+    else {
+        warning.innerHTML = '';
+        tasks.push({ Name: new_task.value, checker: false });
+        new_task.value = '';
+        display();
+    }
+
 })
+
